aea054db45a5ef02b9998cdd59e5d026b015e23d
junbangg <wnstjr4620@naver.com>
2020-01-14 12:21:19 +0900

    Initial Commit

---
+++ flex/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  flex
+//
+//  Created by Jun suk Bang on 2020/01/14.
+//  Copyright © 2020 Jun Bang. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
