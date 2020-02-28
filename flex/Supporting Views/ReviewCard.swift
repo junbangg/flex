c8610b689ec1b04b1d108a602a8e306a533bb425
junbangg <wnstjr4620@naver.com>
2020-02-28 12:31:28 +0900

    final test for merging

--- flex/Supporting Views/ReviewCard.swift
+++ flex/Supporting Views/ReviewCard.swift
@@ -12,6 +12,8 @@ struct ReviewCard: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
         //testing merge
+        //test to see if this can merge to master branch
+        //currently checkversion branch
     }
 }
 
