df9676397700714c9f5523093a7c58b476021a24
junbangg <wnstjr4620@naver.com>
2020-02-28 11:16:42 +0900

    testing merge

---
+++ flex/Supporting Views/ReviewCard.swift
@@ -0,0 +1,21 @@
+//
+//  ReviewCard.swift
+//  flex
+//
+//  Created by Jun suk Bang on 2020/02/28.
+//  Copyright © 2020 Jun Bang. All rights reserved.
+//
+
+import SwiftUI
+
+struct ReviewCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ReviewCard_Previews: PreviewProvider {
+    static var previews: some View {
+        ReviewCard()
+    }
+}
