0dd1fe47afe3646d1adb1181e4215e0167b5e83f
junbangg <wnstjr4620@naver.com>
2020-03-06 18:55:07 +0900

    test commit

---
+++ flex/VIew Controllers/OutfitCard.swift
@@ -0,0 +1,21 @@
+//
+//  OutfitCard.swift
+//  flex
+//
+//  Created by Jun suk Bang on 2020/03/02.
+//  Copyright © 2020 Jun Bang. All rights reserved.
+//
+
+import SwiftUI
+
+struct OutfitCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OutfitCard_Previews: PreviewProvider {
+    static var previews: some View {
+        OutfitCard()
+    }
+}
